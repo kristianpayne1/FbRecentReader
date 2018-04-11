1030ad14826f830267adb675b29730532b794df2
kristianpayne1 <kjp29@kent.ac.uk>
2018-04-11 01:17:21 +0100

    Project start
    Hello world!

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ *
+ * @author Kristian J. Payne
+ * @version 1.0
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
